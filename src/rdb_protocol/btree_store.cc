a6346183fe7b957d4dcb4a6411a8134ac565b5c4
Ian Lee <ian.rob.lee@gmail.com>
2018-09-08 09:55:55 +0100

    Keep formatting consistent

--- src/rdb_protocol/btree_store.cc
+++ src/rdb_protocol/btree_store.cc
@@ -854,10 +854,8 @@ void store_t::drop_sindex(uuid_u sindex_id) THROWS_NOTHING {
     if (!found) {
         // The index got dropped by someone else. That's ok, there's nothing left for us
         // to do.
-
         sindex_block.reset_buf_lock();
         txn->commit();
-
         return;
     }
 
